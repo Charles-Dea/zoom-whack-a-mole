b1405fbd560b583b73170958a9024730b9dc7070
a2k6 <adrian@panezic.com>
2025-09-06 16:59:13 -0500

    Added intro

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://MainScene.tscn")
+	get_tree().change_scene_to_file("res://intro.tscn")
 
 
 func _on_credits_pressed() -> void:
